5fc9ebd530ebecd2df822a4c8ed3a05a522f92fe
GuoKai <badkaikai@gmail.com>
2012-11-21 17:40:45 +0800

    update Query.py

--- Query.py
+++ Query.py
@@ -39,9 +39,12 @@ do_dict = {
 }
 
 class Query(object):
-    def __init__(self, table_name = None):
+    def __init__(self, table_name = None, db = None):
         if not table_name == None:
             self.table_name = table_name
+
+        if not db == None:
+            self.db = db
 
         self.__reset()
         #self.db = Db()
